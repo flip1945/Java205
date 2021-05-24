9fb0eed20bb9ae8fd3f9c1d6f27f60b55f83f201
flip1945 <77256607+flip1945@users.noreply.github.com>
2021-05-24 16:02:00 +0900

    Create Member, Calculator class
    create Member class
    create Calculator class

---
+++ java_project/MyProject/src/Calculator.java
@@ -0,0 +1,30 @@
+
+public class Calculator {
+	
+	public int add(int x, int y) {
+		return x + y;
+	}
+	
+	public int sub(int x, int y) {
+		return x - y;
+	}
+	
+	public int mul(int x, int y) {
+		return x * y;
+	}
+	
+	public int div(int x, int y) {
+		return x / y;
+	}
+
+	public static void main(String[] args) {
+		Calculator cal = new Calculator();
+		
+		System.out.println(cal.add(10, 2));
+		System.out.println(cal.sub(10, 2));
+		System.out.println(cal.mul(10, 2));
+		System.out.println(cal.div(10, 2));
+
+	}
+
+}
---
+++ java_project/MyProject/src/Member.java
@@ -0,0 +1,12 @@
+
+public class Member {
+	
+	static void printMyName() {
+		System.out.println("채호연");
+	}
+	
+	public static void main(String[] args) {
+		printMyName();
+	}
+	
+}

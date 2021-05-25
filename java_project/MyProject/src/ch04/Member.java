532e3ec3b9450c2a000159c53f9664645e3e6085
flip1945 <77256607+flip1945@users.noreply.github.com>
2021-05-25 15:35:19 +0900

    Update Member.java

--- java_project/MyProject/src/ch04/Member.java
+++ java_project/MyProject/src/ch04/Member.java
@@ -1,5 +1,7 @@
 package ch04;
 
+import java.util.Scanner;
+
 public class Member {
 	
 	private static int curYear = 2021;
@@ -28,15 +30,34 @@ public class Member {
 			System.out.println("20세 이상만 무료로 건강검진이 가능합니다.");
 		}
 	}
+	
+	public static int getIntInput(Scanner scanner) {
+		return scanner.nextInt();
+	}
 
 	public static void main(String[] args) {
 		
+		Scanner scanner = new Scanner(System.in);
 		Member member = new Member();
 		
-		member.checkFluVaccine(2000);
-		member.checkMeicalCheck(2002);
-		
-		member.checkMeicalCheck(1980);
+		while (true) {
+			System.out.print("태어난 연도를 입력해주세요(0 입력시 종료) : ");
+			int birthYear = getIntInput(scanner);
+			
+			System.out.println(2022 - birthYear);
+			
+			if (birthYear == 0) {
+				break;
+			}
+			
+			System.out.println("---------독감예방 접종---------");
+			member.checkFluVaccine(birthYear);
+			System.out.println();
+			
+			System.out.println("---------건강검진 대상---------");
+			member.checkMeicalCheck(birthYear);
+			System.out.println();
+		}
 
 	}
 

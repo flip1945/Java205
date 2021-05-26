2edb5ef8de954eddeb69101f4ea7fff3a71e138d
flip1945 <77256607+flip1945@users.noreply.github.com>
2021-05-26 19:53:47 +0900

    Update PersonManager.java

--- java_project/MyProject/src/ch07/PersonManager.java
+++ java_project/MyProject/src/ch07/PersonManager.java
@@ -9,11 +9,11 @@ public class PersonManager {
 		List<Person> people = new ArrayList<Person>();
 		
 		people.add(new Person("홍길동", "901218-1234567"));
-		people.add(new Male("이순신", "801212-1424147", "축구"));
+		people.add(new Male("이순신", "151212-3424147", "축구"));
 		people.add(new Male("강감찬", "941212-1424147", "농구"));
 		people.add(new Female("유관순", "801011-2424147", "독서"));
 		people.add(new Female("이선덕", "000911-2424147", "걷기"));
-		people.add(new Female("이세종", "210111-3424147", "자기"));
+		people.add(new Female("이세종", "210111-4424147", "자기"));
 		
 		for (Person person : people) {
 			person.hi();

cb6dcdf319d1268f54b5282a4ec763eaaf4ec9bf
flip1945 <77256607+flip1945@users.noreply.github.com>
2021-05-31 13:14:23 +0900

    Update CalculatorMain.java
    Update main(input number)

--- java_project/MyProject/src/ch08/CalculatorMain.java
+++ java_project/MyProject/src/ch08/CalculatorMain.java
@@ -1,18 +1,25 @@
 package ch08;
 
+import java.util.Scanner;
+
 public class CalculatorMain {
 
 	public static void main(String[] args) {
 		Calculator cal1 = new ImplementCal();
+		
+		Scanner s = new Scanner(System.in);
+		
+		System.out.print("첫 번째 숫자를 입력해주세요 : ");
+		int n1 = s.nextInt();
+		System.out.print("두 번째 숫자를 입력해주세요 : ");
+		int n2 = s.nextInt();
 
-		int a = 10;
-		int b = 2;
+		System.out.println("두 숫자의 합 : " + cal1.add(n1, n2));
+		System.out.println("두 숫자의 차 : " + cal1.substract(n1, n2));
+		System.out.println("두 숫자의 곱 : " + cal1.multiply(n1, n2));
+		System.out.println("두 숫자의 나누기 : " + cal1.divide(n1, n2));
 
-		System.out.println(cal1.add(a, b));
-		System.out.println(cal1.substract(a, b));
-		System.out.println(cal1.multiply(a, b));
-		System.out.println(cal1.divide(a, b));
-
+		s.close();
 	}
 
 }

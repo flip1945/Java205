c6b08a8d51fbfe3c460ed45d4c37e47a24460afd
flip1945 <77256607+flip1945@users.noreply.github.com>
2021-08-07 10:58:40 +0900

    create OpenProject

---
+++ spring_project/OpenProject/src/main/java/com/bitcamp/op/jdbc/DriverLoader.java
@@ -0,0 +1,30 @@
+package com.bitcamp.op.jdbc;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.ServletConfig;
+
+public class DriverLoader extends HttpServlet {
+	private static final long serialVersionUID = -1658668375376163313L;
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+
+		String params = config.getInitParameter("driver");
+
+		try {
+			if (params != null) {
+				Class.forName(params);
+				System.out.println("드라이버 로드 성공");
+			} else {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				System.out.println("드라이버 로드 성공");
+			}
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로드 실패");
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ spring_project/OpenProject/src/main/java/com/bitcamp/op/member/dao/MemberDao.java
@@ -0,0 +1,11 @@
+package com.bitcamp.op.member.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberDao {
+
+	public void login() {
+		
+	}
+}

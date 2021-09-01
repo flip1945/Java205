50edb1922a1580ff8248cb29137d549057e6776d
flip1945 <77256607+flip1945@users.noreply.github.com>
2021-09-01 12:49:57 +0900

    refactor: controller 파일 수정

--- spring_project/SinChonBeer/src/main/java/com/bitcamp/sc/controller/IndexController.java
+++ spring_project/SinChonBeer/src/main/java/com/bitcamp/sc/controller/IndexController.java
@@ -1,13 +1,20 @@
 package com.bitcamp.sc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
-	@RequestMapping("/")
+	@RequestMapping("/index")
 	public String index() {
 		return "index";
+	}
+	
+	@RequestMapping("/")
+	public String thymeleaf(Model model) {
+//		model.addAttribute("name", "Chae Ho Yeon");
+		return "thymeleaf";
 	}
 }
--- spring_project/SinChonBeer/src/main/java/com/bitcamp/sc/pay/controller/PayController.java
+++ spring_project/SinChonBeer/src/main/java/com/bitcamp/sc/pay/controller/PayController.java
@@ -1,13 +1,15 @@
 package com.bitcamp.sc.pay.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PayController {
 	
 	@RequestMapping("/shop/pay")
-	public String shopPay() {
+	public String shopPay(Model model) {
+		model.addAttribute("userName", "CHY");
 		return "shop_payment";
 	}
 }

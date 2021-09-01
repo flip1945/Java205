a51d8a586b90f9795798ce028bd512777b777836
flip1945 <77256607+flip1945@users.noreply.github.com>
2021-09-01 20:27:11 +0900

    Create Order.java

---
+++ spring_project/SinChonBeer/src/main/java/com/bitcamp/sc/pay/domain/Order.java
@@ -0,0 +1,14 @@
+package com.bitcamp.sc.pay.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Order {
+	private Long oidx;
+	private String id;
+	private String pw;
+}

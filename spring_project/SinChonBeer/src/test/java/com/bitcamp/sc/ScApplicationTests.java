42c268fcfe61b582d10aeaae9e5a44b2df0f140c
flip1945 <77256607+flip1945@users.noreply.github.com>
2021-09-01 12:49:11 +0900

    test: ScApplication 테스트 추가

--- spring_project/SinChonBeer/src/test/java/com/bitcamp/sc/ScApplicationTests.java
+++ spring_project/SinChonBeer/src/test/java/com/bitcamp/sc/ScApplicationTests.java
@@ -1,13 +1,36 @@
 package com.bitcamp.sc;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ScApplicationTests {
+	
+	private DataSource dataSource;
+	private Connection conn;
+	
+	@Autowired
+	ScApplicationTests(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	public void testConnection() throws SQLException {
+		System.out.println(dataSource);
+		conn = dataSource.getConnection();
+		
+		System.out.println(conn);
+		conn.close();
 	}
 
 }
